a80c03b6f9e65113fad8eb12847f4b2d69e5334f
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-05-19 23:18:25 +0300

    урок 7

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 "use strict";
 
-const arr = ['325', '26', '755', '104', '47', '789', '377'];
-
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
-    console.log(arr[i]);
+const weak = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+const date = new Date();
+const options = { weekday: 'long' };
+const dayOfWeek = date.toLocaleString('ru-RU', options); 
+for (let day of weak) {
+  let textDay = document.createElement('p');
+  document.body.appendChild(textDay);
+  textDay.textContent = day; 
+  textDay.style.fontSize = "64px";
+  if (day === "суббота" || day === "воскресенье") {
+    textDay.style.fontWeight = "900";
+  }
+  if (day === dayOfWeek) {
+    textDay.style.fontStyle = "italic";
   }
 }
 
-nextNum:
-for (let i = 2; i <= 100; i++) { 
-
-  for (let j = 2; j < i; j++) { 
-    if (i % j == 0) continue nextNum; 
-  }
-  console.log("Делители этого числа 1 и " + i);
-}
